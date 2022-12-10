f3f74b368c81e2b1349000b29bd9844cb5509bcd
JuanmaEiroa <eiroajm@gmail.com>
2022-12-10 10:59:16 -0300

    Preentrega 2 - Añadido pedido final y métodos de pago con arrays y métodos de búsqueda y modificación

--- proyecto/assets/scripts/menu.js
+++ proyecto/assets/scripts/menu.js
@@ -1,9 +1,8 @@
 //MENU - TABLA DE PRECIOS
 class Producto {
-  constructor(nombre, precio){
-    this.nombre = nombre,
-    this.precio = precio;
-  }
+  constructor(nombre, precio) {
+    (this.nombre = nombre), (this.precio = precio);
+  };
 }
 
 let alitas = new Producto("Alitas de Pollo", 900);
@@ -18,6 +17,15 @@ let cerveza = new Producto("Porrón de Cerveza", 600);
 //TOTAL
 let totalPedido = 0;
 
+//RESUMEN DE PEDIDO
+let pedidoFinal = [];
+function showPedido() {
+  console.log(
+    "Su pedido actualmente se compone de: " + pedidoFinal.join(", ")
+  );
+}
+
+//INICIO
 alert(
   "Bienvenido al sistema de pedidos de Los Pollos Hermanos. Para tomar su pedido, ingrese el código del producto deseado, que se encontrará entre paréntesis antes de la descripción del producto."
 );
@@ -26,17 +34,20 @@ alert(
 let platoPrincipal;
 do {
   platoPrincipal = prompt(
-    "Elija su plato principal:\n\t•(ALI) - Alitas de pollo fritas x6 = $900$\n\t•(PECH) - Pechuga de pollo crispy x2 = $1200\n\t•(HAMB) - Hamburguesa de pollo frita completa = $1500"
+    "Elija su plato principal:\n\t•(ALI) - Alitas de pollo fritas x6 = $900\n\t•(PECH) - Pechuga de pollo crispy x2 = $1200\n\t•(HAMB) - Hamburguesa de pollo frita completa = $1500"
   ).toUpperCase();
   switch (platoPrincipal) {
     case "ALI":
       totalPedido = totalPedido + alitas.precio;
+      pedidoFinal.push(alitas.nombre);
       break;
     case "PECH":
       totalPedido = totalPedido + pechuga.precio;
+      pedidoFinal.push(pechuga.nombre);
       break;
     case "HAMB":
       totalPedido = totalPedido + hamburg.precio;
+      pedidoFinal.push(hamburg.nombre);
       break;
     default:
       alert(
@@ -50,8 +61,9 @@ do {
   platoPrincipal != "HAMB"
 );
 
+showPedido();
 console.log(
-  "El precio total del pedido con el plato principal es de: " +
+  "El precio total del pedido (Plato principal) es de: " +
     totalPedido.toString()
 );
 
@@ -59,17 +71,20 @@ console.log(
 let acompanamiento;
 do {
   acompanamiento = prompt(
-    "Elija su acompañamiento:\n\t•(PAPAS) - Porción de papas fritas = $500$\n\t•(AROS) - Porción de aros de cebolla = $650\n\t•(SIN) - Sin acompañamiento"
+    "Elija su acompañamiento:\n\t•(PAPAS) - Porción de papas fritas = $500\n\t•(AROS) - Porción de aros de cebolla = $650\n\t•(SIN) - Sin acompañamiento"
   ).toUpperCase();
   switch (acompanamiento) {
     case "PAPAS":
       totalPedido = totalPedido + papas.precio;
+      pedidoFinal.push(papas.nombre);
       break;
     case "AROS":
       totalPedido = totalPedido + arosCebolla.precio;
+      pedidoFinal.push(arosCebolla.nombre);
       break;
     case "SIN":
       totalPedido = totalPedido + 0;
+      pedidoFinal.push("Sin acompañamiento");
       break;
     default:
       alert(
@@ -83,6 +98,7 @@ do {
   acompanamiento != "SIN"
 );
 
+showPedido();
 console.log(
   "El precio total del pedido (Plato principal + acompañamiento) es de: " +
     totalPedido.toString()
@@ -97,15 +113,19 @@ do {
   switch (bebida) {
     case "AGUA":
       totalPedido = totalPedido + agua.precio;
+      pedidoFinal.push(agua.nombre);
       break;
     case "GASEO":
       totalPedido = totalPedido + gaseosa.precio;
+      pedidoFinal.push(gaseosa.nombre);
       break;
     case "CERV":
       totalPedido = totalPedido + cerveza.precio;
+      pedidoFinal.push(gaseosa.nombre);
       break;
     case "SIN":
       totalPedido = totalPedido + 0;
+      pedidoFinal.push("Sin bebida");
       break;
     default:
       alert(
@@ -120,16 +140,18 @@ do {
   bebida != "SIN"
 );
 
+showPedido();
 console.log(
   "El precio total del pedido (Plato principal + acompañamiento + bebida) es de: " +
     totalPedido.toString()
 );
 
 //SUBTOTAL DE PEDIDO
-
-alert("El total de su pedido es de: " + totalPedido.toString() + " pesos.");
+alert("Su pedido final se compone de:\n\t" + pedidoFinal.join(".\n\t") + ".\n\nEl total de su pedido es de: $" + totalPedido.toString() + ".");
 
 //MÉTODO DE PAGO y DESCUENTO
+let paymentOpt = ["efe", "transf", "tarj"];
+
 alert(
   "Elija su método de pago a continuación. El pago en efectivo tendrá un 10% de descuento en su pedido final. El pago mediante transferencia bancaria tendrá un 5% de descuento en su pedido final."
 );
@@ -139,21 +161,19 @@ let metodoPago;
 function calcDescuento() {
   do {
     metodoPago = prompt(
-        "Elija el método de pago:\n\t•(EFE) - Efectivo\n\t•(TRANSF) - Transferencia\n\t•(TARJ) - Tarjeta de crédito/débito"
-      ).toLowerCase();
-    if (metodoPago === "efe") {
+      "Elija el método de pago:\n\t•(EFE) - Efectivo\n\t•(TRANSF) - Transferencia\n\t•(TARJ) - Tarjeta de crédito/débito"
+    ).toLowerCase();
+    if (metodoPago === paymentOpt[0]) {
       valorDescuento = 10;
-    } else if (metodoPago === "transf") {
+    } else if (metodoPago === paymentOpt[1]) {
       valorDescuento = 5;
-    } else if (metodoPago === "tarj") {
+    } else if (metodoPago === paymentOpt[2]) {
       valorDescuento = 0;
     } else {
       alert("El método de pago elegido no es válido. Intente nuevamente.");
     }
   } while (
-    metodoPago != "efe" &&
-    metodoPago != "transf" &&
-    metodoPago != "tarj"
+    paymentOpt.includes(metodoPago) === false
   );
   totalPedido = parseFloat(totalPedido * (1 - valorDescuento / 100)).toFixed(2);
 }
